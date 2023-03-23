34c8f0b59a3f3fec82f40f5cbd07238410c4b516
byuranann <127001277+byuranann@users.noreply.github.com>
2023-03-23 11:31:27 +0700

    Add files via upload

---
+++ EP4-CSV.py
@@ -0,0 +1,42 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+import csv
+
+GUI = Tk()
+GUI.title('แบบฟอร์มสมัครเรียน Python 101')
+GUI.geometry('500x400')
+#
+l1 = Label(GUI,text='แบบฟอร์มสมัครเรียน Python 101',font=('Angsana New',30),fg='blue')
+l1.place(x=55,y=20)
+
+l2 = Label(GUI,text='ชื่อ',font=('Angsana New',30),fg='blue')
+l2.place(x=50,y=100)
+l3 = Label(GUI,text='นามสกุล',font=('Angsana New',30),fg='blue')
+l3.place(x=50,y=150)
+#
+box1 = Entry(GUI)
+box2 = Entry(GUI)
+
+box1.place(x=200, y=120)
+box2.place(x=200, y=170)
+
+def Button2():
+    name = box1.get()
+    lname = box2.get()
+
+    with open('data.csv', mode='a',encoding='utf-8', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([name, lname])
+    text = 'บันทึกสำเร็จ'
+    messagebox.showinfo('ยืนยันการสมัคร',text)
+    box1.delete(0,END)
+    box2.delete(0,END)
+
+fb1 = Frame(GUI)
+fb1.place(x=200,y=300)
+b2 = ttk.Button(fb1,text='บันทึก', command=Button2)
+b2.pack(ipadx = 20,ipady = 20)
+
+
+GUI.mainloop()
